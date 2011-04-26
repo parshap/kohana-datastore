aa1c3658f4c0f5905189a24cf077f6edf152d4c3
Parsha Pourkhomami <parshap+git@gmail.com>
2011-04-26 02:34:26 -0700

    adding replace option for mongo update

--- classes/kohana/datastore/mongo/query/update.php
+++ classes/kohana/datastore/mongo/query/update.php
@@ -3,15 +3,22 @@
 abstract class Kohana_Datastore_Mongo_Query_Update
 	extends Datastore_Query_Update {
 
+	protected $_replace = FALSE;
+
+	public function replace(bool $replace)
+	{
+		$this->_replace = $replace;
+	}
+
 	public function execute($datastore = NULL)
 	{
 		$criteria = Datastore_Mongo_Query_Helper::_compile_where_criteria(
 			$this->_where
 		);
 
-		$object = array(
-			'$set' => $this->_set,
-		);
+		$object = $this->_replace
+			? $this->_set
+			: array('$set' => $this->_set);
 
 		Datastore_Mongo::instance($datastore)
 			->get_collection($this->_what)
@@ -19,5 +26,12 @@ abstract class Kohana_Datastore_Mongo_Query_Update
 				'upsert' => FALSE,
 				'multiple' => TRUE,
 			));
+	}
+
+	public function reset()
+	{
+		$this->_replace = FALSE;
+
+		parent::reset();
 	}
 }
