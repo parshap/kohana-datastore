67f82a84b68eca671e44792a0cb5a5a9b5d23688
Parsha Pourkhomami <parshap+git@gmail.com>
2011-08-04 03:14:40 -0700

    Adding `<`, `<=`, `>` and `>=` operators for mongo

--- classes/kohana/datastore/mongo/query/helper.php
+++ classes/kohana/datastore/mongo/query/helper.php
@@ -2,7 +2,24 @@
 
 abstract class Kohana_Datastore_Mongo_Query_Helper {
 
-	protected $_where_ops = array('=');
+	public static $_where_ops = array(
+		'=',
+		'!=',
+		'>',
+		'>=',
+		'<',
+		'<=',
+		'exists',
+	);
+
+	protected static $_mongo_ops = array(
+		'!=' => '$ne',
+		'>' => '$gt',
+		'>=' => '$gte',
+		'<' => '$lt',
+		'<=' => '$lte',
+		'exists' => '$exists',
+	);
 
 	public static function compile_where_criteria(array $where)
 	{
@@ -12,10 +29,32 @@ abstract class Kohana_Datastore_Mongo_Query_Helper {
 		{
 			list($field, $op, $value) = $w;
 
+			// Default this field's criteria to an array so we can simply add
+			// any advanced operators to it. Using the equality operator will
+			// overwrite the array value and can cause confusing inconsistent
+			// behavior depending on the order of the where() calls.
+			if ( ! array_key_exists($field, $criteria))
+			{
+				$criteria[$field] = array();
+			}
+
+			// @todo: Throw an error if an equality operator is mixed with
+			// advanced operator operators?
+
 			switch($op)
 			{
 				case '=':
 					$criteria[$field] = $value;
+				break;
+
+				case '!=':
+				case 'exists':
+				case '>':
+				case '>=':
+				case '<':
+				case '<=':
+					$mongo_op = self::$_mongo_ops[$op];
+					$criteria[$field] += array($mongo_op => $value);
 				break;
 			}
 		}
